f4de3ddf15ca82ecf8f9a0f370a6edc507f41a68
Michael Allen <Michael.Allen@BarefootChurch.com>
2019-11-19 00:06:57 -0500

    Add project for Misc Jobs, and add some we borrowed from other churches.

---
+++ Plugins/com.barefootchurch.MiscJobs/Jobs/FixCombinedGivers.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Quartz;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace com.bricksandmortarstudio.FixCombinedGivers
+{
+    [DisallowConcurrentExecution]
+    [SlidingDateRangeField( "Date Range", "Apply to Person records created within this time frame. If not set then all records are considered.", false, "", enabledSlidingDateRangeTypes: "Previous, Last, Current, DateRange" )]
+    public class FixCombinedGivers : IJob
+    {
+        public void Execute( IJobExecutionContext context )
+        {
+            var dataMap = context.JobDetail.JobDataMap;
+            RockContext rockContext = null;
+            IQueryable<GroupMember> familyMembers = null;
+            var dateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( dataMap.GetString( "DateRange" ) ?? "-1||" );
+            int peopleUpdated = 0;
+            var familyGroupType = GroupTypeCache.Get( Rock.SystemGuid.GroupType.GROUPTYPE_FAMILY );
+
+            do
+            {
+                if ( familyMembers != null )
+                {
+                    foreach ( var familyMember in familyMembers.OrderBy( f => f.Id ).Take( 100 ).ToList() )
+                    {
+                        familyMember.Person.GivingGroupId = familyMember.GroupId;
+                        peopleUpdated += 1;
+                    }
+                    rockContext.SaveChanges();
+                }
+
+                rockContext = new RockContext();
+
+                familyMembers = new GroupMemberService( rockContext )
+                    .Queryable( "Group,Person" )
+                    .Where( g => g.Group.GroupType.Id == familyGroupType.Id &&
+                          ( g.Person.GivingGroupId == 0 || g.Person.GivingGroupId == null ) );
+
+                if ( dateRange.Start.HasValue )
+                {
+                    familyMembers = familyMembers.Where( g => g.Person.CreatedDateTime >= dateRange.Start );
+                }
+
+                if ( dateRange.End.HasValue )
+                {
+                    familyMembers = familyMembers.Where( g => g.Person.CreatedDateTime < dateRange.End );
+                }
+            } while ( familyMembers.Any() );
+
+            context.Result = string.Format( "Combined giving on {0} {1}", peopleUpdated, peopleUpdated == 1 ? "person" : "people" );
+        }
+    }
+}
---
+++ Plugins/com.barefootchurch.MiscJobs/README.md
@@ -0,0 +1,5 @@
+Most of these came from other churches.
+
+- Fix Missing Giving Group - [Bricks and Mortar + Shepherd Church](https://github.com/ShepherdDev/FixMissingGivingGroupJob/blob/feature-dh-daterange/FixCombinedGivers.cs)
+- Match Email Preferences - [CentralAZ](https://github.com/CentralAZ/Rock-CentralAZ/blob/central-develop/com.centralaz.GeneralJobs/Jobs/MatchEmailPreference.cs)
+- Cleanup Scheduled Transactions [Shepherd Church](https://github.com/ShepherdDev/rock-misc/blob/master/Jobs/CleanupScheduledTransactions.cs)
