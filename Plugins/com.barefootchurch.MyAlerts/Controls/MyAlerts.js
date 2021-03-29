5de283f6b54a0cd7c7d9665d98d68e9cdbf2f013
Michael Allen <michael.allen@barefootchurch.com>
2021-03-29 17:07:21 -0400

    Don't position the top with Javascript since it should always be in the same spot.

--- Plugins/com.barefootchurch.MyAlerts/Controls/MyAlerts.js
+++ Plugins/com.barefootchurch.MyAlerts/Controls/MyAlerts.js
@@ -3,7 +3,6 @@
     var $myAlerts = $( '.my-alerts' );
     if ( $profilePhoto.length !== 0 && $myAlerts.length !== 0 ) {
         $myAlerts.css({
-            top: $profilePhoto.offset().top - 10,
             left: $profilePhoto.offset().left + $profilePhoto.width() - ($myAlerts.width() / 2),
         });
     }
