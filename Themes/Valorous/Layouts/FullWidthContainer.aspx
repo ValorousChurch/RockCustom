544900668aec439d6e4d45ae40a9a3d42f4a8593
Michael Allen <Michael.Allen@BarefootChurch.com>
2022-01-20 17:28:11 -0500

    Initial import - Compass v1.3

---
+++ Themes/Valorous/Layouts/FullWidthContainer.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" MasterPageFile="Site.Master" AutoEventWireup="true" Inherits="Rock.Web.UI.RockPage" %>
+<%@ Import Namespace="Rock" %>
+<%@ Import Namespace="Rock.Model" %>
+<%@ Import Namespace="Rock.Web.UI" %>
+<%@ Import Namespace="Rock.Web.Cache" %>
+
+<asp:Content ID="ctMain" ContentPlaceHolderID="main" runat="server">
+
+    <!-- Start Content Area -->
+    <asp:PlaceHolder runat="server">
+        <% var page = PageCache.Get( ( ( RockPage ) Page ).PageId ); %>
+        <% var showPageTitle = page.PageDisplayTitle; %>
+        <% var showPageBreadcrumbs = page.PageDisplayBreadCrumb; %>
+        <% if(showPageTitle) { %>
+            <header class="page bg-primary exclude-nav">
+                <div class="fluid-container">
+                    <div class="row header-content">
+                        <div class="col-lg-12 header-content-inner">
+                            <!-- Page Title -->
+                            <h1 class="section-heading"><Rock:PageIcon ID="PageIcon" runat="server" /> <Rock:PageTitle ID="PageTitle" runat="server" /></h1>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        <% } else { %>
+            <header class="exclude-nav"></header>
+        <% } %>
+        <% if(showPageBreadcrumbs) { %>
+            <div class="breadcrumb-container">
+                <Rock:PageBreadCrumbs ID="PageBreadCrumbs" runat="server" />
+            </div>
+        <% } %>
+    </asp:PlaceHolder>
+
+    <!-- Start Content Area -->
+    <main class="container my-8 w-full xs:max-w-6xl md:max-w-screen-xl mx-auto">
+
+		<Rock:Zone Name="Feature" runat="server" />
+
+        <!-- Ajax Error -->
+        <div class="alert alert-danger ajax-error" style="display:none">
+            <p><strong>Error</strong></p>
+            <span class="ajax-error-message"></span>
+        </div>
+
+        <div class="row no-gutter">
+            <div class="col-md-12">
+                <Rock:Zone Name="Main" runat="server" />
+            </div>
+        </div>
+
+        <div class="row no-gutter">
+            <div class="col-md-12">
+                <Rock:Zone Name="Section A" runat="server" />
+            </div>
+        </div>
+
+        <div class="row no-gutter">
+            <div class="col-md-4">
+                <Rock:Zone Name="Section B" runat="server" />
+            </div>
+            <div class="col-md-4">
+                <Rock:Zone Name="Section C" runat="server" />
+            </div>
+            <div class="col-md-4">
+                <Rock:Zone Name="Section D" runat="server" />
+            </div>
+        </div>
+
+        <!-- End Content Area -->
+	</main>
+</asp:Content>
---
+++ Themes/Valorous/Scripts/compass.js
@@ -0,0 +1,145 @@
+(function($) {
+
+	var $mainNav = $("#mainNav");
+	$('#compass-nav').on('show.bs.collapse', function () {
+		$mainNav.addClass("navOpen");
+	})
+	$('#compass-nav').on('hide.bs.collapse', function () {
+		$mainNav.removeClass("navOpen");
+	})
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+	var backTop = document.getElementsByClassName('js-compass-top')[0],
+		// browser window scroll (in pixels) after which the "back to top" link is shown
+		offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offsetOpacity = 1200,
+		scrollDuration = 700,
+		scrolling = false;
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+			event.preventDefault();
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? addClass(backTop, 'compass-top--show') : removeClass(backTop, 'compass-top--show', 'compass-top--fade-out');
+		( windowTop > offsetOpacity ) && addClass(backTop, 'compass-top--fade-out');
+		scrolling = false;
+	}
+
+	function scrollTop(duration) {
+	    var start = window.scrollY || document.documentElement.scrollTop,
+	        currentTime = null;
+
+	    var animateScroll = function(timestamp){
+	    	if (!currentTime) currentTime = timestamp;
+	        var progress = timestamp - currentTime;
+	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
+	        window.scrollTo(0, val);
+	        if(progress < duration) {
+	            window.requestAnimationFrame(animateScroll);
+	        }
+	    };
+
+	    window.requestAnimationFrame(animateScroll);
+	}
+
+	Math.easeInOutQuad = function (t, b, c, d) {
+ 		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
+		t--;
+		return -c/2 * (t*(t-2) - 1) + b;
+	};
+
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+
+})(jQuery); // End of use strict
