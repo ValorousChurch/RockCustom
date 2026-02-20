8e273793494103712c340136b0eaddd5d479800c
Michael Allen <michael@vulpinesolutions.net>
2026-02-20 13:38:47 -0500

    MyAlerts - Remove old unused plugin

--- Plugins/com.barefootchurch.MyAlerts/Controls/MyAlerts.js
+++
@@ -1,9 +0,0 @@
-﻿Sys.Application.add_load( function () {
-    var $profilePhoto = $('.loginstatus .profile-photo' );
-    var $myAlerts = $( '.my-alerts' );
-    if ( $profilePhoto.length !== 0 && $myAlerts.length !== 0 ) {
-        $myAlerts.css({
-            right: $(window).width() - $profilePhoto.offset().left - $profilePhoto.outerWidth() - $myAlerts.outerWidth() / 2
-        });
-    }
-});
